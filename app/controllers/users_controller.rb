310883813d220ffcaaade918ceedc7d6e3855295
Thilo Rusche <thilorusche@gmail.com>
2022-05-06 09:41:40 +0100

    user edit with country drop-down

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+    @user.update(user_params)
+    redirect_to edit_user_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :email, :country_code)
   end
 end
