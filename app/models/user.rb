a312b02bac6199652143eda494b670aad4ec7080
Thilo Rusche <thilorusche@gmail.com>
2023-06-09 12:31:37 +0100

    added basic spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  def full_name
+    # TODO
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "assigns full name" do
+    user = User.create(email: "foo@bar.com", first_name: "John", last_name: "Doe", country_code: "IE")
+    expect(user.full_name).to eq("John Doe")
+  end
 end
