398e310e7f296cc304a15baf6ecdd79f46b1574d
Thilo Rusche <thilorusche@gmail.com>
2023-07-25 14:51:42 +0100

    using join for full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   before_save :set_full_name
 
   def set_full_name
-    self.full_name = "#{first_name} #{last_name}"
+    self.full_name = [first_name, last_name].join(" ")
   end
 end
